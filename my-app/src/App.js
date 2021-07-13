a97f4b3bee17b3e93738311e8fc046963f426ccf
Abhigyani Anand <abhigyani.97@gmail.com>
2021-07-13 21:22:02 +0530

    Project Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VisNetwork from './vis/vis';
+
+function App() {
+  return (
+    <div className="App">
+      <VisNetwork></VisNetwork>
+    </div>
+  );
+}
+
+export default App;
